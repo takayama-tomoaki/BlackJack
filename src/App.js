bf02f5a060bd597cca51054e76c758fd0b02e9ff
yuyaraki <b.lagoon.nico.ay@gmail.com>
2024-06-02 12:06:30 +0900

    feat; #2_START ボタン表示の実装。

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { Box, Button, Typography } from "@mui/material";
 import BlackJack from "./BlackJack";
 
 export default function App() {
-  function handleClick() {}
+  const [isGameStart, setIsGameStart] = useState(false);
+
+  function handleClick() {
+    setIsGameStart(true);
+  }
+
   // prettier-ignore
   return (
     <Box className="App">
@@ -15,7 +20,13 @@ export default function App() {
         <Box className={"h2-header"}>on Codesandbox</Box>
       </Typography>
       <Box id="table">
-        {/* Task */}
+      {isGameStart ? (
+          <BlackJack />
+        ) : (
+          <Box className="center-button">
+            <Button variant="contained" onClick={handleClick}>START</Button>
+          </Box>
+        )}
       </Box>
     </Box>
   );
